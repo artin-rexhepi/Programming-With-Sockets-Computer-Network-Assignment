624f4fe4003bf82e196655e4273fb26634e02f14
arbnoradragaj <arbnora.dragaj@student.uni-pr.edu>
2024-11-17 10:51:22 +0100

    Set up server to lisen on specified IP and port

--- src/server/server.php
+++ src/server/server.php
@@ -1,7 +1,11 @@
 <?php
 // Server.php
 $port = 8080; 
-$ip_address = '192.168.1.19';
+$ip_address = '192.168.1.17';
 $server_socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+if (socket_bind($server_socket, $ip_address, $port) === false) {
+    die("Lidhja dështoi: " . socket_strerror(socket_last_error($server_socket)) . "\n");
+}
 socket_close($server_socket);
 ?>
