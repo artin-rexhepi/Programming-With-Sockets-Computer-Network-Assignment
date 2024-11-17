3b6c6f22ec0cb4700686240f69c616ab5dfa24e9
arbiosmorina <121387936+Arbios1@users.noreply.github.com>
2024-11-17 10:59:35 +0100

    Add basic error handling for socket creation and binding

--- src/server/server.php
+++ src/server/server.php
@@ -1,14 +1,14 @@
-<?php 
- $port = 8080;
- $ip_address = '192.168.1.8';
-  $server_socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP); 
-  socket_bind($server_socket, $ip_address, $port); 
-  while (true)
-   {
-     $buf = ''; $from = ''; 
-     $port_from = 0; 
-     socket_recvfrom($server_socket, $buf, 1024, 0, $from, $port_from); 
-     echo "Mesazh i pranuar nga $from:$port_from: $buf\n";
-   }
-    socket_close($server_socket);
- ?>
+<?php
+// Server.php
+$port = 8080; 
+$ip_address = '192.168.1.18';
+$server_socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+if ($server_socket === false) {
+    die("Nuk u krijua socket: " . socket_strerror(socket_last_error()) . "\n");
+}
+
+if (socket_bind($server_socket, $ip_address, $port) === false) {
+    die("Lidhja dështoi: " . socket_strerror(socket_last_error($server_socket)) . "\n");
+}
+socket_close($server_socket);
+?>
