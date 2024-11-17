b2a1417a3e886a57f771172bd382ef488aabe412
arbnoradragaj <arbnora.dragaj@student.uni-pr.edu>
2024-11-17 10:57:18 +0100

    Implement basic message exchange between client and server

--- src/client/client.php
+++ src/client/client.php
@@ -1,6 +1,8 @@
-<?php
-$server_ip = '192.168.1.18'; 
-$port = 8080;
+<?php 
+$server_ip = '192.168.1.8'; 
+$port = 8080; 
 $client_socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-socket_close($client_socket);
-?>
+ echo "Shkruani mesazhin për serverin: "; $message = trim(fgets(STDIN)); 
+ socket_sendto($client_socket, $message, strlen($message), 0, $server_ip, $port); 
+ socket_close($client_socket); 
+ ?>
